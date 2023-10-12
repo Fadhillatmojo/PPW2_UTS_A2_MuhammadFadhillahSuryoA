86174c719f7f41b3b990643fd78d73e0ee86f74e
User <fadhillatmojo@gmail.com>
2023-10-12 08:19:28 +0700

    Nomer 2 Done UTS

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $primaryKey = 'id';
+	protected $fillable = ['code', 'name', 'quantity', 'price', 'description'];
 }
